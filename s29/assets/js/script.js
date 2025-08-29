6dac8f3190a0a1cf27ef0e0bda6d45fb28f3cf35
kiarasandovalCTC <kiasandovalrosas@crackthecode.la>
2025-08-28 23:13:13 -0500

    bucles

---
+++ s29/assets/js/script.js
@@ -0,0 +1,13 @@
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(numbers);
+console.log(`El tamaño del array es: ${numbers.length}`);
+console.log(`El primer elemento es: ${numbers[0]}`);
+console.log(`El último elemento es: ${numbers[numbers.length - 1]}`);
+let fruits = ["strawberry", "apple", "banana", "orange"];
+console.log(fruits);
+
+
+for (let i = 0; i <= 26; i+=3) {
+  console.log(i);
+}
